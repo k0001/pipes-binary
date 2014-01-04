7642a41e7c1476d502e5d61758f98ae49f3ff572
Gabriel Gonzalez <Gabriel439@gmail.com>
2014-01-05 05:05:50 +0700

    Added 'Get' and 'Put' functions and 'ByteOffset' functions
    As requested by @k0001

--- src/Pipes/Binary.hs
+++ src/Pipes/Binary.hs
@@ -12,6 +12,14 @@ module Pipes.Binary (
     , decode
     , decoded
 
+    -- * Byte Offsets
+    , decodeL
+    , decodedL
+
+    -- * Get and Put
+    , encodePut
+    , decodeGetL
+
     -- * Types
     , DecodingError(..)
 
@@ -30,8 +38,10 @@ import Control.Exception (Exception)
 import Control.Monad.Trans.Error (Error)
 import Data.Binary (Binary(..))
 import qualified Data.Binary
-import Data.Binary.Get (ByteOffset)
+import Data.Binary.Get (ByteOffset, Get)
 import qualified Data.Binary.Get as Get
+import Data.Binary.Put (Put)
+import qualified Data.Binary.Put as Put
 import Data.Data (Data, Typeable)
 import Lens.Family2 (Lens')
 import Pipes
@@ -42,21 +52,16 @@ import qualified Pipes.Parse as PP
 
 -- | Convert a value to a byte stream
 encode :: (Monad m, Binary a) => a -> Producer ByteString m ()
-encode a = Pipes.ByteString.fromLazy (Data.Binary.encode a)
+encode a = encodePut (put a)
 {-# INLINABLE encode #-}
 
 -- | Parse a value from a byte stream
 decode :: (Monad m, Binary a) => Parser ByteString m (Either DecodingError a)
-decode = PP.StateT (go id (Get.runGetIncremental Data.Binary.get))
-  where
-    go diffP decoder p = case decoder of
-        Get.Fail _ off str -> return (Left (DecodingError off str), diffP p)
-        Get.Partial k      -> do
-            x <- next p
-            case x of
-                Left   e       -> go diffP (k Nothing) (return e)
-                Right (bs, p') -> go (diffP . (yield bs >>)) (k (Just bs)) p'
-        Get.Done bs _  a   -> return (Right a, yield bs >> p)
+decode = do
+    x <- decodeL
+    return $ case x of
+        Left   e     -> Left  e
+        Right (_, a) -> Right a
 {-# INLINABLE decode #-}
 
 -- | An isomorphism between a stream of bytes and a stream of decoded values
@@ -67,16 +72,68 @@ decoded
 decoded k p0 = fmap from (k (to p0))
   where
     to p = do
-        (x, p') <- lift (PP.runStateT decode p)
+        (x, p') <- lift (PP.runStateT decodeL p)
         case x of
-            Left  e -> return (e, p')
-            Right a -> do
+            Left   e     -> return (e, p')
+            Right (_, a) -> do
                 yield a
                 to p'
     from p = do
         (_, p') <- for p encode
         p'
 {-# INLINABLE decoded #-}
+
+{-| Like 'decode', but also returns the length of input consumed to decode the
+    value
+-}
+decodeL
+    :: (Monad m, Binary a)
+    => Parser ByteString m (Either DecodingError (ByteOffset, a))
+decodeL = decodeGetL get
+{-# INLINABLE decodeL #-}
+
+{-| Like 'decoded', except this tags each decoded value with the length of input
+    consumed to decode the value
+-}
+decodedL
+    :: (Monad m, Binary a)
+    => Lens'
+        (Producer ByteString m e)
+        (Producer (ByteOffset, a) m (DecodingError, Producer ByteString m e))
+decodedL k p0 = fmap from (k (to p0))
+  where
+    to p = do
+        (x, p') <- lift (PP.runStateT decodeL p)
+        case x of
+            Left  e -> return (e, p')
+            Right r -> do
+                yield r
+                to p'
+    from p = do
+        (_, p') <- for p (\(_, a) -> encode a)
+        p'
+{-# INLINABLE decodedL #-}
+
+-- | Like 'encode', except this uses an explicit 'Put'
+encodePut :: (Monad m) => Put -> Producer ByteString m ()
+encodePut m = Pipes.ByteString.fromLazy (Put.runPut m)
+{-# INLINABLE encodePut #-}
+
+-- | Like 'decodeL', except this uses an explicit 'Get'
+decodeGetL
+    :: (Monad m)
+    => Get a -> Parser ByteString m (Either DecodingError (ByteOffset, a))
+decodeGetL m = PP.StateT (go id (Get.runGetIncremental m))
+  where
+    go diffP decoder p = case decoder of
+        Get.Fail _ off str -> return (Left (DecodingError off str), diffP p)
+        Get.Partial k      -> do
+            x <- next p
+            case x of
+                Left   e       -> go diffP (k Nothing) (return e)
+                Right (bs, p') -> go (diffP . (yield bs >>)) (k (Just bs)) p'
+        Get.Done bs off  a -> return (Right (off, a), yield bs >> p)
+{-# INLINABLE decodeGetL #-}
 
 -- | A 'Get' decoding error, as provided by 'Fail'
 data DecodingError = DecodingError
@@ -94,7 +151,9 @@ instance Error     DecodingError
 
     [From "Data.Binary"] 'Binary'
 
-    [From "Data.Binary.Get"] 'ByteOffset'
+    [From "Data.Binary.Put"] 'Put'
+
+    [From "Data.Binary.Get"] 'Get', 'ByteOffset'
 
     [From "Pipes.Parse"] 'Parser'
 
@@ -157,5 +216,3 @@ instance Error     DecodingError
 > Just True
 
 -}
-
-
