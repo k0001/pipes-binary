cca7b78c7bacebfabc818d4d74ad7fa5c921c19c
Renzo Carbonara <gnuk0001@gmail.com>
2014-01-23 16:57:23 -0300

    Cosmetic cleanup

--- src/Pipes/Binary.hs
+++ src/Pipes/Binary.hs
@@ -1,59 +1,74 @@
-{-| @pipes@ utilities for encoding and decoding values as byte streams
+-- | @pipes@ utilities for encoding and decoding values as byte streams
+--
+-- The tutorial at the bottom of this module illustrates how to use this
+-- library.
 
-    The tutorial at the bottom of this module illustrates how to use this
-    library.
--}
-
-{-# LANGUAGE DeriveDataTypeable, RankNTypes #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE RankNTypes         #-}
 
 module Pipes.Binary (
-    -- * Encoders and Decoders
-      encode
-    , decode
-    , decoded
+  -- * Encoding
+    encode
+  -- ** Explicit 'Put'
+  , encodePut
 
-    -- * Byte Offsets
-    , decodeL
-    , decodedL
+  -- * Decoding
+  , decode
+  , decoded
+  -- ** Including lengths
+  , decodeL
+  , decodedL
+  -- ** Explicit 'Get'
+  , decodeGet
+  , decodeGetL
 
-    -- * Get and Put
-    , encodePut
-    , decodeGetL
+  -- * Types
+  , DecodingError(..)
 
-    -- * Types
-    , DecodingError(..)
+  -- * Exports
+  -- $exports
+  , module Data.Binary
+  , module Data.Binary.Get
+  , module Pipes.Parse
+  , module Pipes.ByteString
 
-    -- * Exports
-    -- $exports
-    , module Data.Binary
-    , module Data.Binary.Get
-    , module Pipes.Parse
-    , module Pipes.ByteString
+  -- * Tutorial
+  -- $tutorial
+  ) where
 
-    -- * Tutorial
-    -- $tutorial
-    ) where
-
-import Control.Exception (Exception)
-import Control.Monad.Trans.Error (Error)
-import Data.Binary (Binary(..))
+import           Control.Exception         (Exception)
+import           Control.Monad.Trans.Error (Error)
+import           Data.Binary               (Binary (..))
 import qualified Data.Binary
-import Data.Binary.Get (ByteOffset, Get)
-import qualified Data.Binary.Get as Get
-import Data.Binary.Put (Put)
-import qualified Data.Binary.Put as Put
-import Data.Data (Data, Typeable)
-import Lens.Family2 (Lens')
-import Pipes
-import Pipes.ByteString (ByteString)
+import           Data.Binary.Get           (ByteOffset, Get)
+import qualified Data.Binary.Get           as Get
+import           Data.Binary.Put           (Put)
+import qualified Data.Binary.Put           as Put
+import           Data.Data                 (Data, Typeable)
+import           Lens.Family2              (Lens')
+import           Pipes
+import           Pipes.ByteString          (ByteString)
 import qualified Pipes.ByteString
-import Pipes.Parse (Parser, StateT)
-import qualified Pipes.Parse as PP
+import           Pipes.Parse               (Parser, StateT)
+import qualified Pipes.Parse               as PP
 
--- | Convert a value to a byte stream
+--------------------------------------------------------------------------------
+
+-- | Convert a value to a byte stream.
+--
+-- Keep in mind that a single encode value might be split into many 'ByteString'
+-- chunks, that is, the lenght of the obtained 'Producer' might be greater than
+-- 1.
 encode :: (Monad m, Binary a) => a -> Producer ByteString m ()
-encode a = encodePut (put a)
+encode = encodePut . put
 {-# INLINABLE encode #-}
+
+-- | Like 'encode', except this uses an explicit 'Put'
+encodePut :: (Monad m) => Put -> Producer ByteString m ()
+encodePut = Pipes.ByteString.fromLazy . Put.runPut
+{-# INLINABLE encodePut #-}
+
+--------------------------------------------------------------------------------
 
 -- | Parse a value from a byte stream
 decode :: (Monad m, Binary a) => Parser ByteString m (Either DecodingError a)
@@ -66,63 +81,66 @@ decode = do
 
 -- | An isomorphism between a stream of bytes and a stream of decoded values
 decoded
-    :: (Monad m, Binary a)
-    => Lens' (Producer ByteString m e)
-             (Producer a m (DecodingError, Producer ByteString m e))
+  :: (Monad m, Binary a)
+  => Lens' (Producer ByteString m e)
+           (Producer a m (DecodingError, Producer ByteString m e))
 decoded k p0 = fmap from (k (to p0))
   where
     to p = do
         (x, p') <- lift (PP.runStateT decode p)
         case x of
             Left  e -> return (e, p')
-            Right a -> do
-                yield a
-                to p'
+            Right a -> yield a >> to p'
     from p = do
         (_, p') <- for p encode
         p'
 {-# INLINABLE decoded #-}
 
-{-| Like 'decode', but also returns the length of input consumed to decode the
-    value
--}
+--------------------------------------------------------------------------------
+
+-- | Like 'decode', but also returns the length of input consumed in order to
+-- to decode the value.
 decodeL
-    :: (Monad m, Binary a)
-    => Parser ByteString m (Either DecodingError (ByteOffset, a))
+  :: (Monad m, Binary a)
+  => Parser ByteString m (Either DecodingError (ByteOffset, a))
 decodeL = decodeGetL get
 {-# INLINABLE decodeL #-}
 
-{-| Like 'decoded', except this tags each decoded value with the length of input
-    consumed to decode the value
--}
+-- | Like 'decoded', except this tags each decoded value with the length of
+-- input consumed in order to decode it.
 decodedL
-    :: (Monad m, Binary a)
-    => Lens'
-        (Producer ByteString m e)
-        (Producer (ByteOffset, a) m (DecodingError, Producer ByteString m e))
+  :: (Monad m, Binary a)
+  => Lens' (Producer ByteString m e)
+           (Producer (ByteOffset, a) m (DecodingError, Producer ByteString m e))
 decodedL k p0 = fmap from (k (to p0))
   where
     to p = do
         (x, p') <- lift (PP.runStateT decodeL p)
         case x of
             Left  e -> return (e, p')
-            Right r -> do
-                yield r
-                to p'
+            Right r -> yield r >> to p'
     from p = do
         (_, p') <- for p (\(_, a) -> encode a)
         p'
 {-# INLINABLE decodedL #-}
 
--- | Like 'encode', except this uses an explicit 'Put'
-encodePut :: (Monad m) => Put -> Producer ByteString m ()
-encodePut m = Pipes.ByteString.fromLazy (Put.runPut m)
-{-# INLINABLE encodePut #-}
+--------------------------------------------------------------------------------
 
--- | Like 'decodeL', except this uses an explicit 'Get'
+-- | Like 'decode', except this requires an explicit 'Get' instead of any
+-- 'Binary' instance.
+decodeGet :: (Monad m) => Get a -> Parser ByteString m (Either DecodingError a)
+decodeGet m = do
+    x <- decodeGetL m
+    return $ case x of
+        Left   e     -> Left  e
+        Right (_, a) -> Right a
+{-# INLINABLE decodeGet #-}
+
+-- | Like 'decodeL', except this requires an explicit 'Get' instead of any
+-- 'Binary' instance.
 decodeGetL
-    :: (Monad m)
-    => Get a -> Parser ByteString m (Either DecodingError (ByteOffset, a))
+  :: (Monad m)
+  => Get a -> Parser ByteString m (Either DecodingError (ByteOffset, a))
 decodeGetL m = PP.StateT (go id (Get.runGetIncremental m))
   where
     go diffP decoder p = case decoder of
@@ -135,30 +153,36 @@ decodeGetL m = PP.StateT (go id (Get.runGetIncremental m))
         Get.Done bs off  a -> return (Right (off, a), yield bs >> p)
 {-# INLINABLE decodeGetL #-}
 
+--------------------------------------------------------------------------------
+
 -- | A 'Get' decoding error, as provided by 'Fail'
 data DecodingError = DecodingError
-    { peConsumed :: {-# UNPACK #-} !ByteOffset
-      -- ^ Number of bytes consumed before the error
-    , peMessage  :: !String
-      -- ^ Error message
-    } deriving (Show, Read, Eq, Data, Typeable)
+  { peConsumed :: {-# UNPACK #-} !ByteOffset
+    -- ^ Number of bytes consumed before the error
+  , peMessage  :: !String
+    -- ^ Error message
+  } deriving (Show, Read, Eq, Data, Typeable)
 
 instance Exception DecodingError
 instance Error     DecodingError
 
-{- $exports
-    The following types are re-exported from this module for your convenience:
+--------------------------------------------------------------------------------
 
-    [From "Data.Binary"] 'Binary'
+-- $exports
+--
+--  The following types are re-exported from this module for your convenience:
+--
+--  [From "Data.Binary"] 'Binary'
+--
+--  [From "Data.Binary.Put"] 'Put'
+--
+--  [From "Data.Binary.Get"] 'Get', 'ByteOffset'
+--
+--  [From "Data.ByteString"] 'ByteString'
+--
+--  [From "Pipes.Parse"] 'Parser'
 
-    [From "Data.Binary.Put"] 'Put'
-
-    [From "Data.Binary.Get"] 'Get', 'ByteOffset'
-
-    [From "Pipes.Parse"] 'Parser'
-
-    [From "Pipes.ByteString"] 'ByteString'
--}
+--------------------------------------------------------------------------------
 
 {- $tutorial
 
